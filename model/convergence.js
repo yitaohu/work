d7bb96662989eb03cd29fd9d4a3d9df03ad83a62
yitaohu <yitaohu@usc.edu>
2017-11-19 08:30:51 -0500

    update query

--- model/convergence.js
+++ model/convergence.js
@@ -1,7 +1,7 @@
 var db = require('../dbconnection');
 var Convergence = {
     getAllData:function(callback) {
-        return db.query("SELECT DISTINCT Platform FROM feature_fluent ORDER BY Platform", callback);
+        return db.query("SELECT Testdir FROM reg_19 WHERE Testname='2cell-stack_pemfc' AND Platform='win64' AND TimeDateStamp between '2017-11-15 16:50:39' and '2017-11-18 16:50:39'", callback);
     },
     showTable:function(callback) {
         return db.query("SHOW TABLES",callback);
